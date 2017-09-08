e934bdbb8331abbba12a1871c753e3af721f4dc4
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-09-08 14:41:30 -0700

    add average line to charts and caculate average with lodash

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,12 +6,29 @@ class WeatherList extends Component{
 renderWeather(cityData){
   const name = cityData.city.name;
   const temps = cityData.list.map(weather =>weather.main.temp);
+  const pressures = cityData.list.map(weather =>weather.main.pressure);
+  const humidities = cityData.list.map(weather =>weather.main.humidity);
 
   return(
     <tr key={name}>
       <td>{name}</td>
       <td>
-        <Chart data={temps} color="orange"/>
+        <Chart
+          data={temps}
+          color="orange"
+          units="K"/>
+      </td>
+      <td>
+        <Chart
+          data={pressures}
+          color="green"
+          units="hPa"/>
+      </td>
+      <td>
+        <Chart
+          data={humidities}
+          color="blue"
+          units="%"/>
       </td>
     </tr>
   )
