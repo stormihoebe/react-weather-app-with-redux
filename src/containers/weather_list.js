c56b83aacf83eda8dd7d8a76d6fbbbcb867d05d1
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-09-08 15:16:54 -0700

    replace city name with google map of city

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,16 +2,20 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Sparklines, SparklinesLine} from 'react-sparklines';
 import Chart from '../components/chart'
+import GoogleMap from '../components/google_map'
+
 class WeatherList extends Component{
 renderWeather(cityData){
   const name = cityData.city.name;
   const temps = cityData.list.map(weather =>weather.main.temp);
   const pressures = cityData.list.map(weather =>weather.main.pressure);
   const humidities = cityData.list.map(weather =>weather.main.humidity);
-
+  const lon = cityData.city.coord.lon;
+  const lat = cityData.city.coord.lat;
+  // const {lon, lat} = cityData.city.coord
   return(
     <tr key={name}>
-      <td>{name}</td>
+      <td><GoogleMap lon={lon} lat ={lat} /></td>
       <td>
         <Chart
           data={temps}
